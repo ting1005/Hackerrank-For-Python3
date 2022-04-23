8756b1cfd4e62581f593153f677cda0457328766
Chen Ting <61416935+ting1005@users.noreply.github.com>
2022-04-24 00:15:01 +0800

    Create Detect_Floating_Point_Number.py

---
+++ Regex_and_Parsing/Detect_Floating_Point_Number.py
@@ -0,0 +1,18 @@
+import re
+for _ in range(int(input())):
+    string = input()
+    TorF = bool(re.match(r'^[+-]?[0-9]*[.][0-9]+$', string))
+    print(TorF)
+    
+'''
+ re.search() > scans through a string
+ re.match() > only matches
+ 
+Explain : ^[+-]?[0-9]*[.][0-9]+$
+ ^ > Start of Line
+ [+-]? > start with either + or - ('?' is or)
+ [0-9]* > any number from 0-9, it can repeat 0 times ('*' is repeat 0 times or more)([0-9] can equal to \d)
+ [.] > must have '.'
+ [0-9]+ > any number from 0-9, it can repeat 1 times(least 1 times) ('+' is repeat 1 times or more)([0-9] can equal to \d)
+ $ > End of line
+ 
